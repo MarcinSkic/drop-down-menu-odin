8e677904836437b5c551c6d2fe3dc4c781e72f4e
Marcin Skic <marcinskicoficjal@gmail.com>
2023-03-30 18:08:05 +0200

    Add and apply linter

--- generateMenus.js
+++ generateMenus.js
@@ -1,19 +1,19 @@
 let currentMenu = document.querySelector('body');
 
-for (let i = 0; i < 6; i++){
+for (let i = 0; i < 6; i += 1) {
     const container = document.createElement('div');
-    container.style.height = "300px";
+    container.style.height = '300px';
 
     const button = document.createElement('button');
     button.dataset.dropDownMenu = `menu-${i}`;
-    button.classList.add("drop-down-button");
+    button.classList.add('drop-down-button');
     button.textContent = `Menu #${i}`;
 
-    let menu = document.createElement('div');
+    const menu = document.createElement('div');
     menu.id = `menu-${i}`;
-    menu.classList.add('drop-down-container','hidden');
-    
-    container.append(button,menu);
+    menu.classList.add('drop-down-container', 'hidden');
+
+    container.append(button, menu);
     currentMenu.append(container);
     currentMenu = menu;
     console.log(currentMenu);
